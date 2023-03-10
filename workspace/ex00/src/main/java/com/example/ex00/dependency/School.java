dfc05536e4d7aed966964cb9917cc20e352880b9
code-leesooncheol <2sooncheol@gmail.com>
2023-03-10 17:11:48 +0900

    Spring study02

--- workspace/ex00/src/main/java/com/example/ex00/dependency/School.java
+++ workspace/ex00/src/main/java/com/example/ex00/dependency/School.java
@@ -3,8 +3,11 @@ package com.example.ex00.dependency;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @Data
-public class Computer {
-    private int ram;
+public class School {
+    private List<Teacher> teachers;
+    private List<Student> students;
 }
