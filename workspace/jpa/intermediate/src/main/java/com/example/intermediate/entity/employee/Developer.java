de6ca7020fb3e62353ea7eb4ff9195f09892037b
LeeSoonCheol <2sooncheol@gmail.com>
2023-04-18 20:32:26 +0900

    JPA study

--- workspace/jpa/intermediate/src/main/java/com/example/intermediate/entity/employee/Developer.java
+++ workspace/jpa/intermediate/src/main/java/com/example/intermediate/entity/employee/Developer.java
@@ -1,6 +1,5 @@
 package com.example.intermediate.entity.employee;
 
-
 import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,13 +12,34 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "TBL_DEVELOPER")
-@Getter @Setter @ToString
-@DynamicInsert // 필드에 Null이 들어가 있다면 INSERT 쿼리에 해당 필드를 제외해준다.
-@DynamicUpdate // 필드에 Null이 들어가 있다면 UPDATE SET절에 해당 필드를 제외해준다.
+@Getter @Setter @ToString(callSuper = true)
+@DynamicInsert // 필드에 null이 들어가 있다면 INSERT 쿼리에 해당 필드를 제외해준다.
+@DynamicUpdate // 필드에 null이 들어가 있다면 UPDATE SET절에 해당 필드를 제외해준다.
 @DiscriminatorValue("dev")
-public class Developer extends Employee{
-    @NotNull private Integer developinglevel;
-    @ColumnDefault(value = "0")  // 컬럼의 초기값 설정, @DynamicInsert를 통해 제외된 필드는 설정한 Default 값이 들어간다.
+public class Developer extends Employee {
+    private Integer developingLevel;
+    @ColumnDefault(value = "0") //컬럼의 초기값 설정, @DynamicInsert를 통해 제외된 필드는 설정한 Default 값이 들어간다.
     private Integer projectCount;
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
